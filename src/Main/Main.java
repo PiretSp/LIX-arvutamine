62d70b0deea6299373120ed7803c9db7419c587a
Piret Spitsõn <sppiret@gmail.com>
2016-10-11 22:01:16 +0300

    Update Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String args[]) {
         System.out.println("Alustame arvutamisega!");
-	System.out.println("Auto: Piret");
+	System.out.println("Autor: Piret");
 	System.out.println("Aasta: 2016");
     }
 }
